cbe4642d2e2baac44af5a1ea301a337fb6d7d7b3
Pierre ANCELOT <pierreact@gmail.com>
2014-09-19 22:39:56 +0100

    Cleanup

--- src/kmain.s
+++ src/kmain.s
@@ -1,7 +1,7 @@
 
 ;
 ; O.S. Still without a name... This file has the kernel code...
-; Copyright (C) 2012 - 2014 Pierre ANCELOT
+; Copyright (C) 2012 - 2014 Pierre ANCELOT ∴
 ; 
 ; This program is free software; you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License
